87326c79292e2eedc621ced33333813c31e51105
Dan <105542989+4aniDev@users.noreply.github.com>
2022-11-26 14:08:48 +0500

    Add layers

--- app/src/main/java/ru/chani/github/presentation/MainActivity.kt
+++ app/src/main/java/ru/chani/github/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package ru.chani.github
+package ru.chani.github.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ru.chani.github.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
