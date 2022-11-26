14dd37b2424cb0b294a685bf61f9409cb174393a
Dan <105542989+4aniDev@users.noreply.github.com>
2022-11-26 13:33:06 +0500

    Initial commit

---
+++ app/src/main/java/ru/chani/github/MainActivity.kt
@@ -0,0 +1,12 @@
+package ru.chani.github
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
